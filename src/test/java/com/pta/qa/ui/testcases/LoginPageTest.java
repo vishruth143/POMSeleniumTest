eead45583896dbff0e549ab3009dd309ff7d8e6b
Vishvambruth J T <vishruth.cse@gmail.com>
2025-05-25 00:46:07 +0530

    Improved logging for tests

--- src/test/java/com/pta/qa/ui/testcases/LoginPageTest.java
+++ src/test/java/com/pta/qa/ui/testcases/LoginPageTest.java
@@ -1,18 +1,20 @@
 package com.pta.qa.ui.testcases;
 
-import static org.testng.Assert.assertEquals;
-
 import java.net.MalformedURLException;
 
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
 import com.pta.qa.base.TestBase;
 import com.pta.qa.pages.HomePage;
 import com.pta.qa.pages.LoginPage;
+import com.pta.qa.util.TestUtil;
 
 public class LoginPageTest extends TestBase{
 	Logger log;
@@ -22,12 +24,16 @@ public class LoginPageTest extends TestBase{
 	public LoginPageTest() {
 		super();
 		log = Logger.getLogger(LoginPageTest.class);
-	}	
+	}
+	
+	@BeforeSuite
+	public void reportSetup() {
+		initializeReport();
+	}
 	
 	@BeforeMethod
 	public void setup() throws MalformedURLException {
-		initialization();
-		initializeReport();
+		initialization();		
 		loginpage = new LoginPage();
 	}
 	
@@ -35,55 +41,85 @@ public class LoginPageTest extends TestBase{
 	@Test(priority=1)
 	public void loginPageTitleTest() {		
         /*
-        Login Page - Test #01 : Verify Practice Test Automation [PTA] Application Title.
+        Login Page - Test #01 : Verify Practice Test Automation [PTA] Application Login Page Title.
         Steps:
         01) Get PTA login page title.
         02) Verify the login page title.
         */
+		String className = new Exception().getStackTrace()[0].getClassName();
+		String methodName = new Exception().getStackTrace()[0].getMethodName();		
+		test = extent.createTest(className+" : "+methodName, "Sign in and verify the page title");
+		test.assignCategory("Login Page");		
+		
 		log.info("*".repeat(100));
-		log.info("Login Page - Test #01: Verify Practice Test Automation [PTA] Application Title.");
+		test.log(Status.INFO, "*".repeat(100));
+		
+		log.info("Login Page - Test #01: Verify Practice Test Automation [PTA] Application Login Page Title.");
+		test.log(Status.INFO, "Login Page - Test #01: Verify Practice Test Automation [PTA] Application Login Page Title.");
+		
 		log.info("*".repeat(100));
+		test.log(Status.INFO, "*".repeat(100));
 		
 		try {			
-			log.info("Step 01: Get PTA login page title.");				
+			log.info("Step 01: Get PTA login page title.");
+			test.log(Status.INFO, "Step 01: Get PTA login page title.");
 			String title = loginpage.get_loginpage_title();
 			
 			log.info("Step 02: Verify the login page title.");
+			test.log(Status.INFO, "Step 02: Verify the login page title.");
 	        Assert.assertEquals(title, "Test Login | Practice Test Automation", "Login page title not matched");
 
-	        log.info("Login Page - Test #01: Verify Practice Test Automation [PTA] Application Title - Passed");
+	        log.info("Login Page - Test #01: Verify Practice Test Automation [PTA] Application Login Page Title - Passed");
+	        test.log(Status.INFO, "Login Page - Test #01: Verify Practice Test Automation [PTA] Application Login Page Title - Passed");
 		}catch(AssertionError ae) {
-			log.info("Login Page - Test #01: Verify Practice Test Automation [PTA] Application Title - Failed");
+			log.info("Login Page - Test #01: Verify Practice Test Automation [PTA] Application Login Page Title - Failed");
+			test.log(Status.INFO, "Login Page - Test #01: Verify Practice Test Automation [PTA] Application Login Page Title - Failed");
 			log.error("Assertion failed: " + ae.getMessage());
 	        throw ae; // Re-throw so the test fails
 		}catch (Exception e) {
-			log.info("Login Page - Test #01: Verify Practice Test Automation [PTA] Application Title - Failed");
+			log.info("Login Page - Test #01: Verify Practice Test Automation [PTA] Application Login Page Title - Failed");
+			test.log(Status.INFO, "Login Page - Test #01: Verify Practice Test Automation [PTA] Application Login Page Title - Failed");
 	        log.error("Exception occurred during test execution", e);
 	        throw e; // Re-throw so the test fails
 	    }		
 	}
 	
 	@Test(priority=2)
-	public void practiceTestAutomationImageTest() {
+	public void practiceTestAutomationImageTest() {		
 		 /*
         Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo.
         Steps:
         01) Verify the PTA application logo is present.        
         */
+		String className = new Exception().getStackTrace()[0].getClassName();
+		String methodName = new Exception().getStackTrace()[0].getMethodName();		
+		test = extent.createTest(className+" : "+methodName, "Sign in and verify the page title");
+		test.assignCategory("Login Page");		
+		
 		log.info("*".repeat(100));
+		test.log(Status.INFO, "*".repeat(100));
+				
 		log.info("Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo.");
+		test.log(Status.INFO, "Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo.");
+				
 		log.info("*".repeat(100));
+		test.log(Status.INFO, "*".repeat(100));
+				
 		try {
 			log.info("Step 01: Verify the PTA application logo is present.");
+			test.log(Status.INFO, "Step 01: Verify the PTA application logo is present.");
 			boolean flag = loginpage.validate_practicetestautomation_img();
 			Assert.assertTrue(flag);
 			log.info("Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo - Passed");
+			test.log(Status.INFO, "Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo - Passed");
 		}catch(AssertionError ae) {
 			log.info("Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo - Failed");
+			test.log(Status.INFO, "Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo - Failed");
 			log.error("Assertion failed: " + ae.getMessage());
 	        throw ae; // Re-throw so the test fails
 		}catch (Exception e) {
 			log.info("Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo - Failed");
+			test.log(Status.INFO, "Login Page - Test #02 : Verify Practice Test Automation [PTA] Application logo - Failed");
 	        log.error("Exception occurred during test execution", e);
 	        throw e; // Re-throw so the test fails
 	    }	
@@ -97,25 +133,43 @@ public class LoginPageTest extends TestBase{
         Steps:
         01) Provide username and password and verify the PTA login.        
         */
+		String className = new Exception().getStackTrace()[0].getClassName();
+		String methodName = new Exception().getStackTrace()[0].getMethodName();		
+		test = extent.createTest(className+" : "+methodName, "Sign in and verify the page title");
+		test.assignCategory("Login Page");		
+		
 		log.info("*".repeat(100));
+		test.log(Status.INFO, "*".repeat(100));
+				
 		log.info("Login Page - Test #03 : Verify Practice Test Automation [PTA] login.");
+		test.log(Status.INFO, "Login Page - Test #03 : Verify Practice Test Automation [PTA] login.");
+				
 		log.info("*".repeat(100));
+		test.log(Status.INFO, "*".repeat(100));
 		try {
 			log.info("Step 01: Provide username and password and verify the PTA login.");
+			test.log(Status.INFO, "Step 01: Provide username and password and verify the PTA login.");
 			homepage =  loginpage.login(prop.getProperty("username"), prop.getProperty("password"));
 			log.info("Login Page - Test #03 : Verify Practice Test Automation [PTA] login - Passed");
+			test.log(Status.INFO, "Login Page - Test #03 : Verify Practice Test Automation [PTA] login - Passed");
 		}catch (Exception e) {
 			log.info("Login Page - Test #03 : Verify Practice Test Automation [PTA] login - Failed");
+			test.log(Status.INFO, "Login Page - Test #03 : Verify Practice Test Automation [PTA] login - Failed");
 	        log.error("Exception occurred during test execution", e);
 	        throw e; // Re-throw so the test fails
 	    }	
-	}
-	
+	}	
 	
 	@AfterMethod
 	public void teardown() {
 		driver.quit();
 		log.info("Close Chrome");
 	}
-
+	
+	@AfterSuite
+	public void reportClosure() {
+		if (TestUtil.extent != null) {
+	        TestUtil.extent.flush();
+	    }
+	}
 }
